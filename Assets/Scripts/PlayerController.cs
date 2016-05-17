01bbd6c3ab244699f744942a714ace0c7d93d69e
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2016-05-17 08:14:02 -0400

    created audio for explosions and background music

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -48,6 +48,8 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
+
+            GetComponent<AudioSource>().Play();
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
         }
     }
