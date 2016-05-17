90c7eef6ced57a2f248767ca722670f2a915cc87
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2016-05-16 21:30:00 -0400

    finished weapon system

---
+++ Assets/Scripts/DestroyByBondary.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBondary : MonoBehaviour
+{
+
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
