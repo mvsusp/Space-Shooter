92ba90004b65de48993ee78a8fdffaf75d205e7e
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2016-05-17 07:15:20 -0400

    created explosions

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
         {
             return;
         }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, transform.rotation);
+        }
+
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
