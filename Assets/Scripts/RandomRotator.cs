1bbd4f5378b7345ebc95ec2352786981b333bec1
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2016-05-17 07:03:15 -0400

    added asteroid

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble;
+
+    void Start() {
+        var rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+        
+    }
+
+}
