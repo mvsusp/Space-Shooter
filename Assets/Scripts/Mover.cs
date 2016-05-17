1ff0c7fa1b4c8114ba42d1c1a092f9d2061e54a0
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2016-05-16 20:45:28 -0400

    created shots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+    void Start()
+    {
+        var rb = GetComponent<Rigidbody>();
+
+        rb.velocity = transform.forward * speed;
+    }
+}
